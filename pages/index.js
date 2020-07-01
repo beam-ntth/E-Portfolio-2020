8fb23378755525531e566c524e65a56e23c5bdeb
beam-ntth <beam4318.t@gmail.com>
2020-07-01 14:44:45 -0700

    Hopefully works this time lol

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,12 @@ export default function Home () {
   return (
     <div className="container">
       <Head>
-        <title>Beam | Main</title>
         <meta property='og:title' content='Natheethorn Teacharuangchit | E-Portfolio' />
-        <meta property='og:image' content='/front-page.png' />
+        <meta property='og:image' content='https://e-portfolio-2020.vercel.app/front-page.png' />
         <meta property='og:description' content='Natheethorn Teacharuangchit E-Portfolio deployed in 2020. Junior at UC Davis. Graduate June 2022. Utilizing Next.js, React.js, and Node.js for this web application' />
         <meta property='og:url' content='https://e-portfolio-2020.vercel.app/' />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <title>Beam | Main</title>
       </Head>
       <Stars />
       <div className='hero'>
